7c7fbac3d9498e6ad3e5eb9604a9665eb295d4ed
minkh93 <minkh93@naver.com>
2019-11-06 19:43:03 +0900

    sal...ryu...jwiu..

--- Get_data.py
+++ Get_data.py
@@ -33,14 +33,13 @@ class get_data:
         return train_input, train_label
     
     def get_test_data():
-        root_path = "/content/proj1/test"
         train_input=[]
         train_label=[]
 
         img_list = os.listdir(root_path)
-        !cd root_path
+        
         for img in img_list:
-            image = cv2.imread('/'+img, cv2.IMREAD_COLOR)
+            image = cv2.imread("/content/proj1/test/"+img, cv2.IMREAD_COLOR)
             train_input.append([np.array(image)])
             train_label.append([np.array(index)])
         return train_input, train_label
