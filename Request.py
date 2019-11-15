33615aacf37370ba27f9214fcaa422e2715840b3
minkh93 <minkh93@naver.com>
2019-11-15 10:10:08 +0900

    In this version, Output of Request.get_data.test() is image data and image name

--- Request.py
+++ Request.py
@@ -19,7 +19,6 @@ class get_data:
     
     def train():
         root_path = "/content/proj1/train/"
-        
         train_input=[]
         train_label=[]
 
@@ -35,11 +34,27 @@ class get_data:
     def test():
         root_path = "/content/proj1/test"
         train_input=[]
+        img_name=[]
 
         img_list = os.listdir(root_path)
         
         for img in img_list:
             image = cv2.imread("/content/proj1/test/"+img, cv2.IMREAD_COLOR)
             train_input.append(image)
-        return np.array(train_input)
+            img_name.append(img)
+        return np.array(train_input),np.array(img_name)
 
+'''
+class image_pre:
+    def randomCrop(img, width, height):
+        assert img.shape[0] >= height
+        assert img.shape[1] >= width
+      
+        x = random.randint(0, img.shape[1] - width)
+        y = random.randint(0, img.shape[0] - height)
+        img = img[y:y+height, x:x+width]
+
+        return img
+
+    def make_input_batch(batchsize,imageArray):
+'''     
