eec949aa4c3d44aa3fbdcae7fcac2f0713baedc9
minkh93 <minkh93@naver.com>
2019-11-19 21:18:16 +0900

    update

--- Request.py
+++ Request.py
@@ -3,7 +3,6 @@
 
 # <a href="https://colab.research.google.com/github/minkh93/MLDL/blob/master/Get_data.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
 
-
 import cv2
 import re
 import numpy as np
@@ -17,9 +16,10 @@ from sklearn.preprocessing import OneHotEncoder
 
 
 class get_data:
+    def __init__(self):
+        print("access to data")
     
-    def train(size=150):
-        self.size = size
+    def train():
         root_path = "/content/proj1/train/"
         
         train_input=[]
@@ -30,13 +30,11 @@ class get_data:
             img_list = os.listdir(path)
             for img in img_list:
                 image = cv2.imread(path+'/'+img, cv2.IMREAD_COLOR)
-                
-                train_input.append(resize(image,(size,size,3)))
+                train_input.append(image)
                 train_label.append(index)
         return np.array(train_input), train_label
     
-    def test(size=150):
-        self.size = size
+    def test():
         root_path = "/content/proj1/test"
         test_input=[]
 
@@ -44,7 +42,5 @@ class get_data:
         
         for img in img_list:
             image = cv2.imread("/content/proj1/test/"+img, cv2.IMREAD_COLOR)
-            test_input.append(reszie(image,(size,size,3)))
+            test_input.append(image)
         return np.array(test_input), img_list
-
-
---
+++ Resize.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <a href="https://colab.research.google.com/github/minkh93/MLDL/blob/master/Get_data.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
+
+
+import cv2
+import re
+import numpy as np
+import pandas as pd
+import os
+import random
+
+from skimage.transform import resize
+from sklearn.preprocessing import OneHotEncoder
+
+def resize_train(size):
+    print("input_size : ", size)
+    root_path = "/content/proj1/train/"
+    
+    train_input=[]
+    train_label=[]
+
+    for index in range(6):
+        path = root_path+str(index)
+        img_list = os.listdir(path)
+        for img in img_list:
+            image = cv2.imread(path+'/'+img, cv2.IMREAD_COLOR)
+            
+            train_input.append(resize(image,(size,size,3)))
+            train_label.append(index)
+    return np.array(train_input), train_label
+
+def resize_test(size):
+    root_path = "/content/proj1/test"
+    test_input=[]
+
+    img_list = os.listdir(root_path)
+
+    for img in img_list:
+        image = cv2.imread("/content/proj1/test/"+img, cv2.IMREAD_COLOR)
+        test_input.append(resize(image,(size,size,3)))
+    return np.array(test_input), img_list
